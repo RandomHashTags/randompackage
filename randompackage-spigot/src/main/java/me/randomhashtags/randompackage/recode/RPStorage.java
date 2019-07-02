ce04df8d7d6e30c961c9f293b7812039319f4c12
RandomHashTags <imrandomhashtags@gmail.com>
2019-07-02 09:21:02 -0500

    v16.4.0 test commit NOTES - testing

--- randompackage-spigot/src/main/java/me/randomhashtags/randompackage/recode/RPStorage.java
+++ randompackage-spigot/src/main/java/me/randomhashtags/randompackage/recode/RPStorage.java
@@ -2,14 +2,26 @@ package me.randomhashtags.randompackage.recode;
 
 import me.randomhashtags.randompackage.recode.utils.AbstractBooster;
 import me.randomhashtags.randompackage.recode.utils.AbstractLootbox;
+import me.randomhashtags.randompackage.recode.utils.AbstractRPFeature;
 
 import java.util.TreeMap;
 
-public class RPStorage {
+public abstract class RPStorage {
+    private static TreeMap<String, AbstractRPFeature> boosters, lootboxes;
 
-    private TreeMap<String, AbstractBooster> boosters = new TreeMap<>();
-    private TreeMap<String, AbstractLootbox> lootboxes = new TreeMap<>();
+    public AbstractBooster getBooster(String identifier) {
+        return boosters != null ? (AbstractBooster) boosters.getOrDefault(identifier, null) : null;
+    }
+    public void addBooster(String identifier, AbstractBooster b) {
+        if(boosters == null) boosters = new TreeMap<>();
+        boosters.put(identifier, b);
+    }
 
-    public AbstractLootbox getLootbox(String identifier) { return lootboxes.getOrDefault(identifier, null); }
-    public void addLootbox(String identifier, AbstractLootbox l) { lootboxes.put(identifier, l); }
+    public AbstractLootbox getLootbox(String identifier) {
+        return lootboxes != null ? (AbstractLootbox) lootboxes.getOrDefault(identifier, null) : null;
+    }
+    public void addLootbox(String identifier, AbstractLootbox l) {
+        if(lootboxes == null) lootboxes = new TreeMap<>();
+        lootboxes.put(identifier, l);
+    }
 }
