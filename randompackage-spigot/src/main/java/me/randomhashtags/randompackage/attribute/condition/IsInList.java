eab20362803e930fccf194636f202c429a00c2ee
RandomHashTags <imrandomhashtags@gmail.com>
2019-10-19 17:06:10 -0500

    v16.5.0 build 59 NOTES - Developing: Database support - Enabled Enchants: Divine Immolation
    - NOTE: Still needs some yml attribute updating
    
    (not intended for usage, only testing)

--- randompackage-spigot/src/main/java/me/randomhashtags/randompackage/attribute/condition/IsInList.java
+++ randompackage-spigot/src/main/java/me/randomhashtags/randompackage/attribute/condition/IsInList.java
@@ -10,6 +10,8 @@ public class IsInList extends AbstractEventCondition implements Listable {
     @Override
     public boolean check(Entity entity, String value) {
         final UUID u = entity.getUniqueId();
-        return list.containsKey(u) && list.get(u).contains(value);
+        final String[] values = value.split(":");
+        final boolean bool = values.length == 1, contains = list.containsKey(u) && list.get(u).contains(values[0]);
+        return contains == (bool || Boolean.parseBoolean(values[1]));
     }
 }
